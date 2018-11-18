8a1caf3192d72da7808434491545b6d55f713c7a
snewcomer <snewcomer24@gmail.com>
2018-11-18 09:00:15 -0800

    Move logic from ember-in-viewport.  `add` `destroy` `unobserver`

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# intersection-observer-admin
-Intersection Observer Admin for better performance
+intersection-observer-admin
+==============================================================================
+![Download count all time](https://img.shields.io/npm/dt/intersection-observer-admin.svg)
+[![npm version](https://badge.fury.io/js/intersection-observer-admin.svg)](http://badge.fury.io/js/intersection-observer-admin)
+
+[![Dependency Status](https://david-dm.org/snewcomer/intersection-observer-admin.svg)](https://david-dm.org/snewcomer/intersection-observer-admin)
+[![devDependency Status](https://david-dm.org/snewcomer/intersection-observer-admin/dev-status.svg)](https://david-dm.org/snewcomer/intersection-observer-admin#info=devDependencies)
+
+Why use this?
+------------------------------------------------------------------------------
+This library is used in [ember-in-viewport](https://github.com/DockYard/ember-in-viewport).  This library is particularly important for re-using the IntersectionObserver API.  [IntersectionObserver.observer](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/observe) can observer multiple elements.  So this library will resuse the IntersectionObserver instance for another element on the page with the same set of observer options and root element.
+
+Installation
+------------------------------------------------------------------------------
+
+```
+npm install intersection-observer-admin --save
+```
+
+Usage
+------------------------------------------------------------------------------
+### API
+
+1. element: DOM Node to observe
+2. enterCallback: Function
+  - callback function to perform logic in your own application
+3. exitCallback: Function
+  - callback function to perform when element is leaving the viewport
+4. scrollableArea: String
+  - used for determining if element should use existing or new IntersectionObserver
+
+```js
+import intersectionObserverAdmin from 'intersection-observer-admin';
+
+intersectionObserverAdmin.add(element, enterCallback, exitCallback, { root, rootMargin: '0px 0px 100px 0px', threshold: 0 });
+
+intersectionObserverAdmin.add(element, enterCallback, exitCallback, { root, rootMargin: '0px 0px 100px 0px', threshold: 0 }, '.my-list');
+
+// Use in cleanup lifecycle hooks (if applicable) from the element being observed
+intersectionObserverAdmin.unobserve(element, observerOptions, scrollableArea);
+
+// Use in cleanup lifecycle hooks of your application as a whole
+// This will remove the in memory data store holding onto all of the observers
+intersectionObserverAdmin.destroy();
+```
+
+Options passed to `add` are the same options available to the Instersection Observer [API](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export class IntersectionObserverAdmin {
+  private DOMRef: any
+
   constructor() {
-    this._DOMRef = new WeakMap();
+    this.DOMRef = new WeakMap();
   }
 
   /**
@@ -12,10 +14,44 @@ export class IntersectionObserverAdmin {
    * @param {Function} enterCallback
    * @param {Function} exitCallback
    * @param {Object} observerOptions
-   * @param {String} [scrollableArea]
+   * @param {String} scrollableArea
    * @public
    */
   add(element, enterCallback, exitCallback, observerOptions, scrollableArea) {
+    if (!element || !observerOptions) {
+      return;
+    }
+    let { root = window } = observerOptions;
+
+    // first find shared root element (window or scrollable area)
+    let potentialRootMatch = this._findRoot(root);
+    // second if there is a matching root, find an entry with the same observerOptions
+    let matchingEntryForRoot = this._determineMatchingElements(observerOptions, potentialRootMatch);
+
+    if (matchingEntryForRoot) {
+      let { elements, intersectionObserver } = matchingEntryForRoot;
+      elements.push({ element, enterCallback, exitCallback });
+      intersectionObserver.observe(element);
+      return;
+    }
+
+    // No matching entry for root in static admin, thus create new IntersectionObserver instance
+    let newIO = new IntersectionObserver(this._setupOnIntersection(observerOptions, scrollableArea).bind(this), observerOptions);
+    newIO.observe(element);
+    let observerEntry = {
+      elements: [{ element, enterCallback, exitCallback }],
+      observerOptions,
+      intersectionObserver: newIO
+    };
+
+    let stringifiedOptions = this._stringifyObserverOptions(observerOptions, scrollableArea);
+    if (potentialRootMatch) {
+      // if share same root and need to add new entry to root match
+      potentialRootMatch[stringifiedOptions] = observerEntry;
+    } else {
+      // no root exists, so add to WeakMap
+      this.DOMRef.set(root, { [stringifiedOptions]: observerEntry });
+    }
   }
 
   /**
@@ -28,9 +64,178 @@ export class IntersectionObserverAdmin {
    * @public
    */
   unobserve(target, observerOptions, scrollableArea) {
+    let { elements = [], intersectionObserver } = this._findMatchingRootEntry(observerOptions, scrollableArea);
+
+    intersectionObserver.unobserve(target);
+
+    // important to do this in reverse order
+    for (let i = elements.length - 1; i >= 0; i--) {
+      if (elements[i] && elements[i].element === target) {
+        elements.splice(i, 1);
+        break;
+      }
+    }
   }
 
+  /**
+   * cleanup data structures and unobserve elements
+   *
+   * @method destroy
+   */
   destroy() {
-    this._DOMRef = null;
+    this.DOMRef = null;
+  }
+
+  /**
+   * use function composition to curry observerOptions
+   *
+   * @method _setupOnIntersection
+   * @param {Object} observerOptions
+   * @param {String} scrollableArea
+   */
+  _setupOnIntersection(observerOptions, scrollableArea) {
+    return function(entries) {
+      return this._onIntersection(observerOptions, scrollableArea, entries);
+    }
+  }
+
+  /**
+   * IntersectionObserver callback when element is intersecting viewport
+   *
+   * @method _onIntersection
+   * @param {Object} observerOptions
+   * @param {String} scrollableArea
+   * @param {Array} ioEntries
+   * @private
+   */
+  _onIntersection(observerOptions, scrollableArea, ioEntries) {
+    ioEntries.forEach((entry) => {
+
+      let { isIntersecting, intersectionRatio } = entry;
+
+      // first determine if entry intersecting
+      if (isIntersecting) {
+        // then find entry's callback in static administration
+        let { elements = [] } = this._findMatchingRootEntry(observerOptions, scrollableArea);
+
+        elements.some((obj) => {
+          if (obj.element === entry.target) {
+            // call entry's enter callback
+            obj.enterCallback();
+            return true;
+          }
+        });
+      } else if (intersectionRatio <= 0) { // exiting viewport
+        // then find entry's callback in static administration
+        let { elements = [] } = this._findMatchingRootEntry(observerOptions, scrollableArea);
+
+        elements.some((obj) => {
+          if (obj.element === entry.target) {
+            // call entry's enter callback
+            obj.exitCallback();
+            return true;
+          }
+        });
+      }
+    });
+  }
+
+  /**
+   * @method _findRoot
+   * @param {Node|window} root
+   * @private
+   * @return {Object} of elements that share same root
+   */
+  _findRoot(root) {
+    return this.DOMRef.get(root);
+  }
+
+  /**
+   * Used for onIntersection callbacks and unobserving the IntersectionObserver
+   * We don't care about observerOptions key order because we already added
+   * to the static administrator or found an existing IntersectionObserver with the same
+   * root && observerOptions to reuse
+   *
+   * @method _findMatchingRootEntry
+   * @param {Object} observerOptions
+   * @param {String} scrollableArea
+   * @return {Object} entry with elements and other options
+   */
+  _findMatchingRootEntry(observerOptions, scrollableArea) {
+    let { root = window } = observerOptions;
+    let matchingRoot = this._findRoot(root) || {};
+    let stringifiedOptions = this._stringifyObserverOptions(observerOptions, scrollableArea);
+    return matchingRoot[stringifiedOptions];
+  }
+
+  /**
+   * Determine if existing elements for a given root based on passed in observerOptions
+   * regardless of sort order of keys
+   *
+   * @method _determineMatchingElements
+   * @param {Object} observerOptions
+   * @param {Object} potentialRootMatch e.g. { stringifiedOptions: { elements: [], ... }, stringifiedOptions: { elements: [], ... }}
+   * @private
+   * @return {Object} containing array of elements and other meta
+   */
+  _determineMatchingElements(observerOptions, potentialRootMatch = {}) {
+    let matchingKey = Object.keys(potentialRootMatch).filter((key) => {
+      let { observerOptions: comparableOptions } = potentialRootMatch[key];
+      return this._areOptionsSame(observerOptions, comparableOptions);
+    })[0];
+
+    return potentialRootMatch[matchingKey];
+  }
+
+  /**
+   * recursive method to test primitive string, number, null, etc and complex
+   * object equality.
+   *
+   * @method _areOptionsSame
+   * @param {Object} observerOptions
+   * @param {Object} comparableOptions
+   * @private
+   * @return {Boolean}
+   */
+  _areOptionsSame(observerOptions, comparableOptions) {
+    // simple comparison of string, number or even null/undefined
+    let type1 = Object.prototype.toString.call(observerOptions);
+    let type2 = Object.prototype.toString.call(comparableOptions);
+    if (type1 !== type2) {
+      return false;
+    } else if (type1 !== '[object Object]' && type2 !== '[object Object]') {
+      return observerOptions === comparableOptions;
+    }
+
+    // complex comparison for only type of [object Object]
+    for (let key in observerOptions) {
+      if (observerOptions.hasOwnProperty(key)) {
+        // recursion to check nested
+        if (this._areOptionsSame(observerOptions[key], comparableOptions[key]) === false) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Stringify observerOptions for use as a key.
+   * Excludes observerOptions.root so that the resulting key is stable
+   *
+   * @param {Object} observerOptions
+   * @param {String} scrollableArea
+   * @private
+   * @return {String}
+   */
+  _stringifyObserverOptions(observerOptions, scrollableArea) {
+    let replacer = (key, value) => {
+      if (key === 'root') {
+        return scrollableArea;
+      }
+      return value;
+    };
+
+    return JSON.stringify(observerOptions, replacer);
   }
 }
